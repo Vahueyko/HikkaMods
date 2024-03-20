8788c4f07671869b8dde1c8d7b64dd8e9acade7a
Vahueyko <113551387+dndndjskxifir@users.noreply.github.com>
2024-03-20 22:24:40 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -10,41 +10,32 @@
 from telethon import events
 from .. import loader, utils
 
-valuegreet = False  # Устанавливаем значение переменной в начале
-
-@loader.tds
 class GreetingMod(loader.Module):
     """Приветствует пользователя"""
 
-    strings = {"name": "Приветствие"}
+    def __init__(self):
+        self.greet_settings = {}  # Привязка к чату
 
     async def greetcmd(self, message):
         """Используй .greet, чтобы поприветствовать пользователя"""
-        sender = await message.get_sender()
-        if sender:
-            await utils.answer(message, "Привет, {}!".format(sender.first_name))
-        else:
-            await utils.answer(message, "Привет!")
+        await self._greet(message)
 
     async def greetoncmd(self, message):
         """Используй .greeton, чтобы включить приветствие"""
-        global valuegreet
-        valuegreet = True
-        await utils.answer(message, "Приветствие включено!")
+        chat_id = message.chat_id
+        self.greet_settings[chat_id] = True
+        await utils.answer(message, "Приветствие включено в этом чате!")
 
     async def greetoffcmd(self, message):
         """Используй .greetoff, чтобы выключить приветствие"""
-        global valuegreet
-        valuegreet = False
-        await utils.answer(message, "Приветствие выключено!")
+        chat_id = message.chat_id
+        self.greet_settings[chat_id] = False
+        await utils.answer(message, "Приветствие выключено в этом чате!")
 
-    async def client_ready(self, client, db):
-        await client.send_message("me", "Я запущен!")
-
-    @loader.unrestricted
-    @loader.ratelimit
     async def watcher(self, message):
-        global valuegreet  # Используем глобальную переменную
         sender = await message.get_sender()
-        if valuegreet and sender and any(word in message.text.lower() for word in ["привет", "здаров", "ку"]):  # Проверяем значение переменной и текст сообщения
+        if not sender:
+            return
+        chat_id = message.chat_id
+        if self.greet_settings.get(chat_id, False) and sender and any(word in message.text.lower() for word in ["привет", "здаров", "ку", "пр"]):
             await message.reply("Привет, {}!".format(sender.first_name))

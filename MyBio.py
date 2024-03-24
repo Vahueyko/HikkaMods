e683757bb8b8c178cda2370f27bb46c1f5ebee85
Vahueyko <113551387+dndndjskxifir@users.noreply.github.com>
2024-03-24 21:55:26 +0300

    Create MyBio.py

---
+++ MyBio.py
@@ -0,0 +1,64 @@
+#made by Vahueyko
+
+from telethon import events
+from .. import loader, utils
+
+class MyBioMod(loader.Module):
+    """Приветствует пользователя"""
+    
+    strings = {"name": "MyBio"}
+        
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "Биография1",
+                None,
+                lambda: "Напишите свое Био",
+            ),
+            loader.ConfigValue(
+                "banner_url1",
+                "https://i.yapx.cc/XQUzA.png",
+                lambda: self.strings("_cfg_banner"),
+                validator=loader.validators.Link(),
+            ),
+            loader.ConfigValue(
+                "banner_url2",
+                "https://i.yapx.cc/XQUzA.png",
+                lambda: self.strings("_cfg_banner"),
+                validator=loader.validators.Link(),
+            ),
+            loader.ConfigValue(
+                "Биография2",
+                None,
+                lambda: "Напишите свое Био",
+            ),
+        )
+        
+           
+    
+    async def mbio1cmd(self, message):
+        """Используй, чтобы прислать свою  первую биографию"""
+        (
+                    {"photo": self.config["banner_url1"]}
+                    if self.config["banner_url1"]
+                    else {}
+                ),
+        await utils.answer_file(
+                message,
+                self.config["banner_url1"],
+                self.config["Биография1"],
+            ),
+    async def mbio2cmd(self, message):
+        """Используй, чтобы прислать свою  вторую биографию"""
+        (
+                    {"photo": self.config["banner_url2"]}
+                    if self.config["banner_url2"]
+                    else {}
+                ),
+        await utils.answer_file(
+                message,
+                self.config["banner_url2"],
+                self.config["Биография2"],
+            ),
+       
+       
